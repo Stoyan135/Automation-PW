314c3c8226125644b8880b71ed231df56f9bac12
Stoyan135 <zlatev55@gmail.com>
2025-03-05 23:50:16 +0200

    Lecuture-7

--- objects/objects.ts
+++ objects/objects.ts
@@ -1,2 +1,72 @@
 
-type Role =  "Manager"
+// Object types with Aliases
+type Role =  {
+   role: "manager" | "founder"
+}
+type Permissions = {
+    permissions: "admin" | "user"
+}
+
+type User = Role & Permissions;
+
+
+const user: User = {
+    role: "founder",
+    permissions: "admin"
+}
+//console.log("userOnject", user);
+
+// Objecy types with Interfaces
+interface Animal {
+    species: string;
+    breed?: string;
+    age: number;
+    weight?: number;
+    name: string;
+}
+
+const animal: Animal = {
+species: "dog",
+age: 5,
+name: "Tom",
+breed: "Frenchie",
+weight: 3
+};
+
+//console.log("animal", animal);
+
+interface Cars {
+    color: string;
+    wheels: number;
+    transmission: "manual" | "automatic";
+    fuelType: "disel" | "gasoline";
+    startEngine: () => string
+}
+
+const car: Cars = {
+    color: "white",
+    wheels: 4,
+    transmission: "manual",
+    fuelType: "disel",
+    startEngine: function() {
+        return 'Start the engine'
+    },
+}
+const carStartEngine = car.startEngine();
+//console.log("car", car);
+
+// Get object keys
+
+// const keys = Object.keys(car) - keys 
+// const keys = Object.values(car) - values
+// console.log("keys", keys);
+
+// Assign two objects in one
+
+const colorObj = {color: 'black'}
+const backgraoundObj = {background: 'white'}
+
+const assigedObj = Object.assign({}, colorObj, backgraoundObj)
+console.log("assigedObj", assigedObj);
+
+export {};
