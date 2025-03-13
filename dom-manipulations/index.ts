14b6aca24fc9123c6130dc2cfbdab41349ddc3de
Stoyan135 <zlatev55@gmail.com>
2025-03-13 14:39:48 +0200

    LLL

--- dom-manipulations/index.ts
+++ dom-manipulations/index.ts
@@ -1,6 +1,7 @@
-const bodyEl = document.body;
+const bodyEl: HTMLElement = document.body;
 bodyEl.style.backgroundColor = "#000";
 bodyEl.style.color = "#fff";
+
 bodyEl.style.border = "2px solid red";
 
 console.log("body background", bodyEl.style.backgroundColor);
@@ -11,9 +12,27 @@ const divEls: HTMLCollectionOf<HTMLDivElement> =
 const btn: HTMLButtonElement = document.getElementsByTagName("button")[0];
 
 btn.style.backgroundColor = "green";
-btn.textContent = "Abount me";
+btn.textContent = "About me";
 
 const headingsTwo: HTMLCollectionOf<HTMLHeadingElement> =
   document.getElementsByTagName("h2");
 
-console.log("divEls", divEls);
+const paragraphs: HTMLCollectionOf<HTMLParagraphElement> =
+  document.getElementsByTagName("p");
+
+const shops: HTMLElement | null = document.getElementById("shops");
+
+const items: HTMLCollectionOf<Element> =
+  document.getElementsByClassName("list-item");
+
+const secondItem: Element = document.getElementsByClassName("list-item")[1];
+
+secondItem.textContent = "Second Element";
+
+const selectedH2 = document.querySelector("h2") as HTMLHeadingElement;
+
+selectedH2.style.color = "blue";
+
+const aboutUsBtn = document.querySelector(".about button") as HTMLButtonElement;
+
+console.log("aboutUsBtn", aboutUsBtn);
