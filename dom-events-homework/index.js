601cc03510a06e99a268705c557699098922e7a8
Stoyan135 <zlatev55@gmail.com>
2025-03-18 17:10:08 +0200

    Homework-Dom-Events

--- dom-events-homework/index.js
+++ dom-events-homework/index.js
@@ -1,33 +1,25 @@
 // Select elements using querySelector
-const addBorderBtn = document.querySelector(
-  "#add-border-btn"
-) as HTMLButtonElement;
-const changeTextBtn = document.querySelector(
-  "#change-text-btn"
-) as HTMLButtonElement;
-const contentSection = document.querySelector(".content") as HTMLElement;
-
+var addBorderBtn = document.querySelector("#add-border-btn");
+var changeTextBtn = document.querySelector("#change-text-btn");
+var contentSection = document.querySelector(".content");
 // Add event listener when button with id "add-border-btn" is clicked
 // and add border style "2px solid red" for section with class name "content"
-addBorderBtn.addEventListener("click", () => {
-  contentSection.style.border = "2px solid red";
+addBorderBtn.addEventListener("click", function () {
+    contentSection.style.border = "2px solid red";
 });
-
 // Add event listener when button with id "change-text-btn" is clicked
 // and add color style "blue" for section with class name "content"
-changeTextBtn.addEventListener("click", () => {
-  contentSection.style.color = "blue";
+changeTextBtn.addEventListener("click", function () {
+    contentSection.style.color = "blue";
 });
-
 // Add event listener when button with id "change-text-btn" is clicked
 // and add backgroundColor style "gray" for section with class name "content"
-changeTextBtn.addEventListener("click", () => {
-  contentSection.style.backgroundColor = "gray";
+changeTextBtn.addEventListener("click", function () {
+    contentSection.style.backgroundColor = "gray";
 });
-
 //Add event listener when section element with class name "content" is hovered
 // this means to use mouseover event type
 // and add box-shadow style  12px 12px 2px 1px rgba(0, 0, 255, 0.2);
-contentSection.addEventListener("mouseover", () => {
-  contentSection.style.boxShadow = "12px 12px 2px 1px rgba(0, 0, 255, 0.2)";
+contentSection.addEventListener("mouseover", function () {
+    contentSection.style.boxShadow = "12px 12px 2px 1px rgba(0, 0, 255, 0.2)";
 });
