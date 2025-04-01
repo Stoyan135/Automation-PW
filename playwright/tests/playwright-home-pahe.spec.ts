56e2027563a720aa689086ef02d15f3c9e1eca77
Stoyan135 <zlatev55@gmail.com>
2025-04-01 20:34:49 +0300

    New changes 01.04

--- playwright/tests/playwright-home-pahe.spec.ts
+++ playwright/tests/playwright-home-pahe.spec.ts
@@ -1,58 +1,63 @@
 import { test, expect } from "@playwright/test";
 
-test("Logo exist", async ({ page }) => {
-  const url = process.env.BASE_URL as string;
-  await page.goto(url);
-  // get image by alt text
-  const logo = page.getByAltText("Playwright logo").first();
+test.describe("Playwright home page testing", () => {
+  test.beforeEach("Navigate to the home page", async ({ page }) => {
+    const url = process.env.BASE_URL as string;
+    await page.goto(url, { timeout: 60000 });
+  });
 
-  await expect(logo).toBeVisible();
-});
+  test("Logo exist", async ({ page }) => {
+    // get image by alt text
+    const logo = page.getByAltText("Playwright logo").first();
+    await expect(logo).toBeVisible();
+  });
 
-test("Heading exist", async ({ page }) => {
-  const url = process.env.BASE_URL as string;
-  await page.goto(url);
+  test("Heading exist", async ({ page }) => {
+    // locate heading one by locator tag name
+    const headingTitle = page.locator("h1");
+    // log in the test resuls located element value
+    console.log((await headingTitle.innerText()).valueOf());
+    await expect(headingTitle).toBeVisible();
+  });
 
-  // locate heading one by locator tag name
-  const headingTitle = page.locator("h1");
-  // log in the test resuls located element value
-  console.log((await headingTitle.innerText()).valueOf);
-  await expect(headingTitle).toBeVisible();
-});
+  test("Nav links exist", async ({ page }) => {
+    // locate nav link Docs by role and text
+    const docsLink = page.getByRole("link", { name: "Docs" });
+    const apiLink = page.getByRole("link", { name: "API" });
 
-test("Nav links exist", async ({ page }) => {
-  const url = process.env.BASE_URL as string;
-  await page.goto(url);
+    await expect(docsLink).toBeVisible();
+    await expect(apiLink).toBeVisible();
+  });
 
-  // locate nav link Docs by role and text
-  const docsLink = page.getByRole("link", { name: "Docs" });
-  const apiLink = page.getByRole("link", { name: "API" });
+  test("Click community link and check the path", async ({ page }) => {
+    // locate nav link Community by role and text
+    const communityLink = page.getByRole("link", { name: "Community" });
+    // clikc the located element
+    await communityLink.click();
+    // expect the current page to have passed url
+    await expect(page).toHaveURL("https://playwright.dev/community/welcome");
 
-  await expect(docsLink).toBeVisible();
-  await expect(apiLink).toBeVisible();
-});
+    const headingTwo = page.getByRole("heading", { name: "Ambassadors" });
+    await expect(headingTwo).toBeVisible();
+  });
 
-test("Click community link and check the path", async ({ page }) => {
-  const url = process.env.BASE_URL as string;
-  await page.goto(url);
+  test("Check if logos list exists", async ({ page }) => {
+    // locate logos list by locator class name synatx
+    const logosListItems = page.locator(".logosList_zAAF li");
+    // expect the count of list items1
+    await expect(logosListItems).toHaveCount(9);
+  });
 
-  // locate nav link Community by role and text
-  const communityLink = page.getByRole("link", { name: "Community" });
-  // clikc the located element
-  await communityLink.click();
-  // expect the current page to have passed url
-  await expect(page).toHaveURL("https://playwright.dev/community/welcome");
+  test("Select Playwright env from the list", async ({ page }) => {
+    const initialEnv = page.getByRole("button", { name: "Node.js" });
 
-  const headingTwo = page.getByRole("heading", { name: "Ambassadors" });
-  await expect(headingTwo).toBeVisible();
-});
+    await initialEnv.hover();
 
-test("Check if logos list exists", async ({ page }) => {
-  const url = process.env.BASE_URL as string;
-  await page.goto(url);
+    const envList = page.locator(".dropdown__menu");
+    await expect(envList).toBeVisible();
 
-  // locate logos list by locator class name synatx
-  const logosListItems = page.locator(".logosList_zAAF li");
-  // expect the count of list items1
-  await expect(logosListItems).toHaveCount(9);
+    const listItem = page.locator("ul.dropdown__menu a").getByText("Python");
+    await listItem.click();
+    await expect(page).toHaveURL("https://playwright.dev/python/");
+  });
 });
