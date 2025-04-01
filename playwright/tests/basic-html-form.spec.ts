bd6dfd0cc4edd1ea3dc3e58d78c4bff450c7b5a6
Stoyan135 <zlatev55@gmail.com>
2025-04-01 22:12:18 +0300

    Changes-01.04.1

---
+++ playwright/tests/basic-html-form.spec.ts
@@ -0,0 +1,42 @@
+import { test, expect } from "@playwright/test";
+
+test("Has h1 title", async ({ page }) => {
+  await page.goto(
+    "https://testpages.herokuapp.com/styled/basic-html-form-test.html"
+  );
+  const title = page.locator("//h1");
+  await expect(title).toHaveText("Basic HTML Form Example");
+});
+
+test("Test username field", async ({ page }) => {
+  await page.goto(
+    "https://testpages.herokuapp.com/styled/basic-html-form-test.html"
+  );
+
+  const usernameInput = page.locator("//input[@name='username']");
+  await expect(usernameInput).toBeVisible();
+  await usernameInput.fill("testUser");
+  await expect(usernameInput).toHaveValue("testUser");
+});
+
+test("Test Checkbox selection", async ({ page }) => {
+  await page.goto(
+    "https://testpages.herokuapp.com/styled/basic-html-form-test.html"
+  );
+
+  const checkboxEl = page.locator('xpath=//input[@value="cb2"]');
+  await expect(checkboxEl).toBeVisible();
+  await checkboxEl.check();
+  await expect(checkboxEl).toBeChecked();
+});
+
+test("Select from the dropdown options", async ({ page }) => {
+  await page.goto(
+    "https://testpages.herokuapp.com/styled/basic-html-form-test.html"
+  );
+
+  const dropdown = page.locator('xpath=//select[@name="dropdown"]');
+  await expect(dropdown).toBeVisible();
+  await dropdown.selectOption("dd2");
+  await expect(dropdown).toHaveValue("dd2");
+});
